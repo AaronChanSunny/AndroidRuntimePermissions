8b416e55afc1c2f9b5aaf6caa8ae930506ffb995
aaronchansunny <aaronchansunny@gmail.com>
2016-02-27 11:35:38 +0800

    use permission dispatcher.

--- app/src/main/java/com/aaron/androidruntimepermissions/MainActivity.java
+++ app/src/main/java/com/aaron/androidruntimepermissions/MainActivity.java
@@ -3,25 +3,26 @@ package com.aaron.androidruntimepermissions;
 import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
 public class MainActivity extends AppCompatActivity {
 
-    private static final int REQUEST_PERMISSION_CALL_PHONE = 1;
     private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
@@ -31,71 +32,55 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         findViewById(R.id.call).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                call10086();
+                MainActivityPermissionsDispatcher.call10086WithCheck(MainActivity.this);
             }
         });
     }
 
-    private void call10086() {
-        Intent intent = new Intent(Intent.ACTION_CALL);
+    @NeedsPermission(Manifest.permission.CALL_PHONE)
+    void call10086() {
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:10086"));
+        startActivity(intent);
+    }
 
-        Uri uri = Uri.parse("tel:" + "10086");
-        intent.setData(uri);
+    @OnShowRationale(Manifest.permission.CALL_PHONE)
+    void showRationaleForCallPhone(final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setTitle("提示")
+                .setMessage("需要授权电话权限")
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        request.cancel();
+                    }
+                })
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        request.proceed();
+                    }
+                })
+                .create()
+                .show();
+    }
 
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission
-                .CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest
-                    .permission.CALL_PHONE)) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("说明")
-                        .setMessage("需要使用电话权限，进行电话测试")
-                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                ActivityCompat.requestPermissions(MainActivity.this, new
-                                        String[]{android.Manifest.permission.CALL_PHONE}, REQUEST_PERMISSION_CALL_PHONE);
-                            }
-                        })
-                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                return;
-                            }
-                        })
-                        .create()
-                        .show();
-            } else {
-                ActivityCompat.requestPermissions(this, new String[]{android
-                        .Manifest.permission.CALL_PHONE}, REQUEST_PERMISSION_CALL_PHONE);
-            }
-        } else {
-            Log.d(TAG, "call phone permission granted.");
-            startActivity(intent);
-        }
+    @OnPermissionDenied(Manifest.permission.CALL_PHONE)
+    void showDeniedForCallPhone() {
+        Toast.makeText(this, "未授权", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnNeverAskAgain(Manifest.permission.CALL_PHONE)
+    void showNeverAskForPhoneCall() {
+        Toast.makeText(this, "不再询问", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        if (requestCode == REQUEST_PERMISSION_CALL_PHONE) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                call10086();
-            } else {
-                Toast.makeText(this, "Permission denied.", Toast.LENGTH_SHORT).show();
-            }
-        }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
     }
 
     @Override
